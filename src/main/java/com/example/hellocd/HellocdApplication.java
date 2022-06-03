cd1314818c6a3d325f4d0dfb816ae5b44512379b
GotGit20 <varun.rajasekaran85@gmail.com>
2022-06-02 22:22:44 -0500

    Fixed imports

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,5 +1,7 @@
 package com.example.hellocd;
 
+import org.springframework.boot.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
