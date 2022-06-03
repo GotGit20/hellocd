57828ad5b0074bc9b8d7162873066ef32720191d
GotGit20 <varun.rajasekaran85@gmail.com>
2022-06-02 22:28:40 -0500

    Fixed annotation issue for REST

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class HellocdApplication {
 
 	@RequestMapping("/")
